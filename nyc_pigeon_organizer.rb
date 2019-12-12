1cec205a777b5c885c8daf53aadf45af7be54996
Paul Hoffart <pahoffart25@gmail.com>
2019-12-12 21:11:32 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,30 +1,18 @@
 def nyc_pigeon_organizer(data)
-  birds = {
-    "Theo" => {
-      :color => ["purple", "grey"],
-      :gender => ["male"],
-      :lives => ["Subway"]
-    },
-    "Peter Jr." => {
-      :color => ["purple", "grey"],
-      :gender => ["male"],
-      :lives => ["Library"]
-    },
-    "Lucky" => {
-      :color => ["purple"],
-      :gender => ["male"],
-      :lives => ["Central Park"]
-    },
-    "Ms. K" => {
-      :color => ["white", "grey"],
-      :gender => ["female"],
-      :lives => ["Central Park"]
-    },
-    "Queenie" => {
-      :color => ["white", "brown"],
-      :gender => ["female"],
-      :lives => ["Subway"]
-    },
-    
-  }
+  pigeon_list = {}
+  data.each do |color_gender_lives, value|
+    value.each do |stats, all_names|
+      all_names.each do |name|
+        if pigeon_list[name] == nil
+          pigeon_list[name] = {}
+        end
+        if pigeon_list[name][color_gender_lives] == nil
+          pigeon_list[name][color_gender_lives] = []
+        end
+        pigeon_list[name][color_gender_lives].push(stats.to_s)
+      end
+    end 
+  end
+  pigeon_list
 end
+
