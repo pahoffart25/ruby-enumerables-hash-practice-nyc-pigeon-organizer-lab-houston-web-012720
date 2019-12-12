de8a2841b738b7bda2642b40bb20b123992c588d
Paul Hoffart <pahoffart25@gmail.com>
2019-12-12 20:57:07 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,7 +1,9 @@
 def nyc_pigeon_organizer(data)
   birds = {
     "Theo" => {
-      
-    }
+      :color => ["purple" "grey"],
+      :gender => ["male"],
+      :lives => ["subway"]
+    },
   }
 end
